275a1ec292659374cfe3ba1acde35ef897559bd1
WinWinparis <156462593+WinWinparis@users.noreply.github.com>
2024-01-13 12:27:17 +0100

    Update and rename style.css to app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MonSiteBasique extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Bonjour tout le monde !</h1>
+        <p>C'est un site basique React, mais il est fonctionnel.</p>
+      </div>
+    );
+  }
+}
+
+export default MonSiteBasique;
